924c26d4511a3a0b8a79394d7ef69ede9ba43265
Juergen Hess <juergen@HUGO>
2015-09-17 22:43:17 -0700

    created app and can call authentification page

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
 from stravalib import Client
-
+import webbrowser
+import requests
 class Predictor:
 
-    client_id_="juergenmhess@gmail.com"
+    client_id_=8127
     def __init__(self):
         self.data_ = []
         self.strava_client_=Client()
         self.url_ = self.strava_client_.authorization_url(client_id=self.client_id_,
-                               redirect_uri='http://127.0.0.1:5000/authorization')
-
+                               redirect_uri='http://127.0.0.1:5000/authorization',scope='write',)
+        #webbrowser.open_new_tab(self.url_)
+        r = requests.get(self.url_)
 if __name__ == "__main__":
     pred=Predictor()
+
 
